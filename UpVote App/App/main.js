badc81835cda079ac3f35b296d1c71f31e7ea0be
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-05-20 11:27:04 +0530

    fix: :lipstick: voting, dynamic binding added : master

--- UpVote App/App/main.js
+++ UpVote App/App/main.js
@@ -9,8 +9,30 @@ const upVoteApp = {
       return this.submissions.sort((a, b) => {
         return b.votes - a.votes;
       });
+      /**
+       *? When the sort() function compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
+       *` If the result is negative a is sorted before b.
+       *` If the result is positive b is sorted before a.
+       *` If the result is 0 no changes are done with the sort order of the two values.
+       */
     },
   },
+  methods: {
+    upVote(submissionId) {
+      const submission = this.submissions.find(
+        (submissions) => submissions.id === submissionId
+      );
+      submission.votes++;
+    },
+  },
+
+  /**
+   *? The find() method returns the value of the first element that passes a test.
+   *` The find() method executes a function for each array element.
+   *` The find() method returns undefined if no elements are found.
+   *` The find() method does not execute the function for empty elements.
+   *` The find() method does not change the original array.
+   */
 };
 
 Vue.createApp(upVoteApp).mount("#app");
