375c634bece9a9e1a4f52998646cbd6d858b00df
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-05-27 15:20:21 +0530

    perf: :building_construction: components added : master

--- UpVote App/App/main.js
+++ UpVote App/App/main.js
@@ -1,3 +1,51 @@
+const submissionComponent = {
+  template: `<div style="display:flex; width:100%">
+          <figure class="media-left">
+            <img
+              class="image is-64x64"
+              v-bind:src="submission.submissionImage"
+            />
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>
+                  <a href="#" class="has-text-info">{{submission.title}}</a>
+                  <span class="tag is-small">{{submission.tags}}</span>
+                </strong>
+                <br />
+                {{submission.description}}
+                <br />
+                <small class="is-size-7">
+                  Submitted by:
+                  <img
+                    class="image is-24x24"
+                    v-bind:src="submission.avatar"
+                    v-bind:class="{ 'red-border': submission.votes >= 25 }"
+                  />
+                </small>
+              </p>
+            </div>
+          </div>
+          <div class="media-right">
+            <span class="icon is-small" v-on:click="upVote(submission.id)">
+              <i class="fa fa-chevron-up"></i>
+              <strong class="has-text-info">{{submission.votes}}</strong>
+            </span>
+          </div>
+          </div>`,
+
+  props: ["submission", "submissions"],
+  methods: {
+    upVote(submissionId) {
+      const submission = this.submissions.find(
+        (submissions) => submissions.id === submissionId
+      );
+      submission.votes++;
+    },
+  },
+};
+
 const upVoteApp = {
   data() {
     return {
@@ -17,14 +65,6 @@ const upVoteApp = {
        */
     },
   },
-  methods: {
-    upVote(submissionId) {
-      const submission = this.submissions.find(
-        (submissions) => submissions.id === submissionId
-      );
-      submission.votes++;
-    },
-  },
 
   /**
    *? The find() method returns the value of the first element that passes a test.
@@ -33,6 +73,10 @@ const upVoteApp = {
    *` The find() method does not execute the function for empty elements.
    *` The find() method does not change the original array.
    */
+
+  components: {
+    "submission-component": submissionComponent,
+  },
 };
 
 Vue.createApp(upVoteApp).mount("#app");
